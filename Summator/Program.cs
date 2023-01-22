23295e68e48cf1c59793b2f3c254ae532965742a
Kaio <Kaio@DESKTOP-F2G4TH6>
2023-01-23 01:39:24 +0200

    Add project files.

---
+++ Summator/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Summator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Summator.Test_SumTwoNumbers();
+            /* Console.WriteLine("Sum equals " + Summator.Sum(new int[] {1, 2, 3})); */
+        }
+    }
+}
